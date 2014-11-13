53f6b1c21fa7e287db89015cfbe886e372ee2929
leotsai <leo.tsai.2010@gmail.com>
2014-11-13 16:39:33 +0800

    refactor statistics project

---
+++ MvcSolution._Libs/MvcSolution.Libs.Statistics/Context/StatisticsDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using MvcSolution.Libs.Statistics.Entities;
+
+namespace MvcSolution.Libs.Statistics.Context
+{
+    public class StatisticsDataContext : DbContext
+    {
+        public DbSet<RequestLog> RequestLogs { get; set; }
+
+        public StatisticsDataContext()
+        {
+            Database.SetInitializer<StatisticsDataContext>(null);
+        }
+    }
+}
