7c08328d9807dcbc2e9d6502f619b58527003058
leotsai <leo.tsai.2010@gmail.com>
2014-11-13 14:20:44 +0800

    refactor

---
+++ MvcSolution.Data/Entities/Setting.cs
@@ -0,0 +1,21 @@
+﻿namespace MvcSolution.Data.Entities
+{
+    public class Setting : EntityBase
+    {
+        public string Key { get; set; }
+        public string Notes { get; set; }
+        public string Value { get; set; }
+
+        public Setting()
+        {
+            
+        }
+
+        public Setting(string key, string value, string notes)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.Notes = notes;
+        }
+    }
+}
