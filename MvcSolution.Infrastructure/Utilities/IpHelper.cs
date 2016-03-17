84c696a75c1a69e3bfc7d01e7df09c595cf856f0
Leo <leo.tsai.2010@gmail.com>
2016-03-17 11:06:42 +0800

    added ip address

--- MvcSolution.Infrastructure/Utilities/IpHelper.cs
+++ MvcSolution.Infrastructure/Utilities/IpHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,40 @@ namespace MvcSolution
     {
         public static string GetAddress(string ip)
         {
-            if (string.IsNullOrWhiteSpace(ip))
+            var url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip;
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var html = client.DownloadString(url);
+                    return Serializer.FromJson<IpEntity>(html).GetAddress();
+                }
+            }
+            catch
             {
                 return string.Empty;
             }
-            return "ip address";
         }
+    }
+
+    internal class IpEntity
+    {
+        public string Code { get; set; }
+        public IpEntityValue Data { get; set; }
+
+        public string GetAddress()
+        {
+            if (this.Data == null)
+            {
+                return string.Empty;
+            }
+            return this.Data.Country + this.Data.City;
+        }
+    }
+
+    internal class IpEntityValue
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
     }
 }
